26b4a17ee622ca9657a4014d362dbd709f2d21de
Rajab Shoukath Kunnath <rajabshoukathis@gmail.com>
2025-04-25 12:51:42 +0530

    Refactor search page to implement course fetching and selection functionality. Replace placeholder content with dynamic course list and integrate loading states. Update layout component by removing unused imports.

--- client/app/(nondashboard)/layout.tsx
+++ client/app/(nondashboard)/layout.tsx
@@ -1,5 +1,4 @@
 import NonDashboardNavbar from "@/components/NonDashboardNavbar";
-import Landing from "@/app/(nondashboard)/landing/page";
 import Footer from "@/components/Footer";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
---
+++ client/components/AccordionSections.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from './ui/accordion'
+import { FileText } from 'lucide-react'
+
+const AccordionSections = ({ sections }: AccordionSectionsProps) => {
+    return (
+        <Accordion type='multiple' className='w-full'>
+            {sections.map((section) => (
+                <AccordionItem key={section.sectionId} value={section.sectionTitle} className='accordion-section'>
+                    <AccordionTrigger className='accordion-section__trigger'>
+                        <h5 className='accordion-section__title'>
+                            {section.sectionTitle}
+                        </h5>
+                    </AccordionTrigger>
+                    <AccordionContent className='accordion-section__content'>
+                        <ul>
+                            {section.chapters.map((chapter) => (
+                                <li className='accordion-section__chapter' key={chapter.chapterId}>
+                                    <FileText className='mr-2 size-4' />
+                                    <span className='text-sm'>{chapter.title}</span>
+                                </li>
+                            ))}
+                        </ul>
+                    </AccordionContent>
+                </AccordionItem>
+            ))}
+        </Accordion>
+    )
+}
+
+export default AccordionSections
