ad8af75f36efa4467b23d3c4516efd81f058e32d
Rajab Shoukath Kunnath <rajabshoukathis@gmail.com>
2025-04-25 16:41:01 +0530

    Update .gitignore to include Clerk configuration and add commented-out CSS rule in globals.css for internal styling adjustments.

---
+++ client/app/(auth)/signin/[[...signin]]/page.tsx
@@ -0,0 +1,12 @@
+import SignInComponent from '@/components/SignIn'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+        <SignInComponent/>
+    </div>
+  )
+}
+
+export default Page
---
+++ client/app/(auth)/signup/[[...signup]]/page.tsx
@@ -0,0 +1,12 @@
+import SignUpComponent from '@/components/SignUp'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+        <SignUpComponent/>
+    </div>
+  )
+}
+
+export default Page
